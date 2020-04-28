8dbb656f4d014e5472997dda0dc111323411d69a
Gustavo Barbosa <gusbdev@gmail.com>
2020-04-28 20:36:58 -0300

    Updates

---
+++ precache-manifest.492a049d58601fedc5e401ab13851a15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e03f65dd38e656a04385f8a56a9ad84",
+    "url": "/noughts-and-crosses/index.html"
+  },
+  {
+    "revision": "dea62a5ef884c7a5cd85",
+    "url": "/noughts-and-crosses/static/css/main.35b309c5.chunk.css"
+  },
+  {
+    "revision": "62f46d3b1b63f286d3de",
+    "url": "/noughts-and-crosses/static/js/2.e8ede047.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noughts-and-crosses/static/js/2.e8ede047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea62a5ef884c7a5cd85",
+    "url": "/noughts-and-crosses/static/js/main.2dea96f0.chunk.js"
+  },
+  {
+    "revision": "3be471d41784e3c3230e",
+    "url": "/noughts-and-crosses/static/js/runtime-main.aaa46902.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e8d31a2d2f27760a05eccf020c265e5.js"
+  "/noughts-and-crosses/precache-manifest.492a049d58601fedc5e401ab13851a15.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/noughts-and-crosses/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
